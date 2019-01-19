db6ad419e0b8583fdd255eaa2c21a45c2996b5fd
DANIELLE PARK <dpark1008@gmail.com>
2019-01-19 03:56:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,11 +5,11 @@ class Artist
 
   def initialize (name)
     @name = name
-    @songs = []
+    @song = []
   end
 
-  def add_song(songs)
-    self.songs << song
+  def add_song(song)
+    self.song << song
   end
 
   def self.all
